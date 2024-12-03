fe5b63a2b0f8bd8c6dd3fe68f1686705374d8d9f
Shashika Virajh <v1rhinosa@sph.com.sg>
2024-12-03 14:16:09 +0530

    Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,24 @@ async function bootstrap() {
       transform: true, // Transform input data to DTO instances (by default input data is a general object type)
     }),
   );
+
+  /** Swagger configuration */
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Base URL: http://localhost:3001')
+    .setTermsOfService('http://localhost:3001/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+    )
+    .addServer('http://localhost:3001')
+    .setVersion('1.0')
+    .build();
+
+  // Instantiate Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 
