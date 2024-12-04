5a84b48e7a32246682a11aafb0ec7b912d38e338
Shashika Virajh <v1rhinosa@sph.com.sg>
2024-12-04 11:52:43 +0530

    Update swagger for create post

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -16,18 +16,31 @@ import {
 } from 'class-validator';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty({
+    example: 'This is a title',
+    description: 'This is the title of the post',
+  })
   @IsString()
   @MinLength(4)
   @MaxLength(512)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: PostType,
+    description: "Possible values, 'post', 'page', 'story', 'series'",
+  })
   @IsEnum(PostType)
   @IsNotEmpty()
   postType: PostType;
 
+  @ApiProperty({
+    description: "For Example - 'my-url'",
+    example: 'my-blog-post',
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(256)
@@ -37,33 +50,78 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: PostStatus,
+    description: "Possible values 'draft', 'scheduled', 'review', 'published'",
+  })
   @IsEnum(PostStatus)
   @IsNotEmpty()
   status: PostStatus;
 
+  @ApiPropertyOptional({
+    description: 'This is the content of the post',
+    example: 'The post content',
+  })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'Serialize your JSON object else a validation error will be thrown',
+    example:
+      '{\r\n "@context": "https://schema.org",\r\n "@type": "Person"\r\n }',
+  })
   @IsOptional()
   @IsJSON()
   schema?: string;
 
+  @ApiPropertyOptional({
+    description: 'Featured image for your blog post',
+    example: 'http://localhost.com/images/image1.jpg',
+  })
   @IsOptional()
   @IsUrl()
   @MaxLength(1024)
   featuredImageUrl?: string;
-  
+
+  @ApiPropertyOptional({
+    description: 'The date on which the blog post is published',
+    example: '2024-03-16T07:46:32+0000',
+  })
   @IsISO8601()
   @IsOptional()
   publishOn?: string;
 
+  @ApiPropertyOptional({
+    description: 'Array of ids of tags',
+    example: ['one', 'two'],
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @MinLength(3, { each: true })
   tags?: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+          description: 'The key can be any string identifier',
+          example: 'sidebarEnabled',
+        },
+        value: {
+          type: 'any',
+          description: 'The value can be any value',
+          example: true,
+        },
+      },
+    },
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
