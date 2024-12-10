0ff501996fb097764678d7a5ba26c8e86d974c3b
Shashika Virajh <v1rhinosa@sph.com.sg>
2024-12-10 10:05:35 +0530

    Create tag

--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Tag } from '../tag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTagDto } from '../dtos/create-tag.dto';
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagsRepository: Repository<Tag>,
+  ) {}
+
+  public async create(createTagDto: CreateTagDto) {
+    const newTag = this.tagsRepository.create(createTagDto);
+    return await this.tagsRepository.save(newTag);
+  }
+}
