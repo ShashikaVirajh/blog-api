082db9535f991dbef955cc979012dd38ecfcf182
Shashika Virajh <v1rhinosa@sph.com.sg>
2024-12-12 11:57:49 +0530

    Configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,10 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// Get the current NODE_ENV
+const CURRENT_ENVIRONMENT = process.env.NODE_ENV;
 
 @Module({
   imports: [
@@ -17,20 +21,29 @@ import { MetaOptionsModule } from './meta-options/meta-options.module';
     AuthModule,
     TagsModule,
     MetaOptionsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !CURRENT_ENVIRONMENT
+        ? '.env'
+        : `.env.${CURRENT_ENVIRONMENT}`,
+    }),
 
     TypeOrmModule.forRootAsync({
-      imports: [],
-      inject: [],
-      useFactory: () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        entities: [],
-        autoLoadEntities: true, // Automatically load entities defined in feature modules and synchronize the database schema.
-        synchronize: true,
-        port: 5432,
-        username: 'postgres',
-        password: 'Postgres#12',
-        host: 'localhost',
-        database: 'blog',
+        // entities: [],
+
+        synchronize: configService.get('DATABASE_SYNCHRONIZE'),
+        // Automatically load entities defined in feature modules and synchronize the database schema.
+        autoLoadEntities: configService.get('DATABASE_AUTOLOAD_ENTITIES'),
+
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        host: configService.get('DATABASE_HOST'),
+        database: configService.get('DATABASE_NAME'),
       }),
     }),
   ],
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -4,10 +4,14 @@ import { Repository } from 'typeorm';
 import { User } from '../user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
   constructor(
+    // Injecting config service
+    private readonly configService: ConfigService,
+    // Injecting user repository
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
@@ -27,6 +31,9 @@ export class UsersService {
     limit: number,
     page: number,
   ) {
+    const environment = this.configService.get<string>('S3_BUCKET_NAME');
+    console.log(environment);
+
     return [
       {
         firstName: 'John',
